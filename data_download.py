089025a3cdfb1d293b3b0eac5626358f648d12e2
Aaron Meyer <agmeyer4@gmail.com>
2024-05-02 16:16:13 -0600

    separated funcs, reconfigured

--- data_download.py
+++ data_download.py
@@ -15,6 +15,7 @@ the user if there is less than a defined amount (default = 6Tb) of space on the 
 The dataset from 2019 Month01 to 2021 Month08 is approximately 4.5 Terabytes. 
 ****
 
+TODO Don't download if the data is already there
 '''
 
 ##################################################################################################################################
@@ -23,6 +24,7 @@ import os
 import subprocess
 import shutil
 import glob
+import noaa_csl_funcs as ncf
 
 ##################################################################################################################################
 # Define Classes
@@ -127,7 +129,7 @@ class NOAA_CSL_download:
         year (int) : integer year to download
         month (int) : integer month to download
         '''
-        month_str = self.month_int_to_str(month) #convert the integer month into the month string used by NOAA
+        month_str = ncf.month_int_to_str(month) #convert the integer month into the month string used by NOAA
         if (year == 2020) & (sector in ['area_onroad_gasoline','area_onroad_diesel','area_offroad']): #in 2020, there are two versios of the FIVE inventory, choose the one defined in the class instatiation
             year_str = str(year)+self.bau_or_covid
         else:
@@ -203,7 +205,7 @@ class NOAA_CSL_download:
         '''
 
         prefix_temp = self.sector_details[sector]['extracted_preday_prefix'] #get the prefix template for the sector
-        prefix = replace_all_strs(prefix_temp,{'MonthXX':month_str}) #get the real prefix by replacing the month (if necessary)
+        prefix = ncf.replace_all_strs(prefix_temp,{'MonthXX':month_str}) #get the real prefix by replacing the month (if necessary)
         dir_dict = {} #initialize the data dict
         if sector == 'point': #for the "point" sector, we must define different "subsectors" that will rise to top level sectors when moving
             for new_subsec, old_subsec in self.point_extract_structure.items():
@@ -298,16 +300,6 @@ class NOAA_CSL_download:
         print(f'Deleting tree {tree_path}')
         shutil.rmtree(tree_path)
     
-    def month_int_to_str(self,month_int):
-        '''Converts a month integer to a two character string
-        
-        Args: 
-        month_int (int) : the integer of the month (1,2,12,etc)
-        
-        Returns (str) : a string of form "MonthXX" where XX is the two digit month (1 --> Month01)'''
-
-        return f'Month{month_int:02d}'
-    
     def url_fname_create(self,url_fname_temp,year_str,month_str):
         '''Creates the filename in the NOAA server for a specific year/month given the template
         
@@ -319,106 +311,29 @@ class NOAA_CSL_download:
         Returns (str) : a string with the year and month replaced representing the filename in the NOAA server'''
 
         dic = {'year':year_str,'MonthXX':month_str}
-        return replace_all_strs(url_fname_temp,dic)
+        return ncf.replace_all_strs(url_fname_temp,dic)
 
-##################################################################################################################################
-# Define Functions
-def replace_all_strs(text,dic):
-    '''Replaces all strings contained in text matching the keys of dic with the values of dic
-    
-    Args:
-    text (str) : the string we want to replace stuff in
-    dic (dict) : a dictionary with keys as what should be replaced in text and values with what the key should be replaced with
-    
-    Returns :
-    text (str): the new text with keys replaced
-    '''
 
-    for key,value in dic.items():
-        text = text.replace(key,value)
-    return text
-
-def check_space(path,excep_thresh='8Tb'):
-    '''Checks the amount of space on a filesystem given a path, and raise an error if the amount of space is below the threshold
-    
-    Args:
-    path (str) : path to check how much room there would be based on the filesystem
-    excep_thresh (str) : threshold below which to raise an exception, defaults '8T'
-    
-    Returns:
-    usage_details (tuple) : output of shutil.disk_usage(path)
-    
-    Raises:
-    MemoryError : if the amount of space on the filesystem containing the path is below the excep_thresh
-    '''
-
-    disk_usage = shutil.disk_usage(path)
-    total_bytes,used_bytes,free_bytes = int(disk_usage.total), int(disk_usage.used), int(disk_usage.free)
-    thresh_bytes = human_to_bytes(excep_thresh)
-    if free_bytes < thresh_bytes:
-        print(f'Path: {path}')
-        print(f'Total space: {bytes_to_human(total_bytes)}')
-        print(f'Used space: {bytes_to_human(used_bytes)}')
-        print(f'Free space: {bytes_to_human(free_bytes)}')
-        print(f'Required space (a user defined property): {excep_thresh}\n')
-        raise MemoryError(f'There is less than {excep_thresh} in the destination filesystem. Ensure there is enough room for your data download, or change the threshold')
-    else:
-        print(f'Sufficient space detected in {path}')
-        print(f'Found {bytes_to_human(free_bytes)} free space')
-        return disk_usage
-
-def human_to_bytes(human_readable, units=['b', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb']):
-    '''Makes a human readable storage size into a bytes integer
-    #TODO Different definitions for this and the other direction (1000 bytes per kb vs 1024)
-    
-    Args:
-    human_readable (str) : a human readable string representing space (something like 2.1Gb, or 5Tb)
-    units (list) : list of strings defining the units 
-    
-    Returns:
-    bytes (int) : integer representing how many bytes the human readable string is
-    '''
-
-    # Split the input string into value and unit
-    value_str = ''.join([ch for ch in human_readable if ch.isdigit() or ch == '.'])
-    unit_str = ''.join([ch for ch in human_readable if not ch.isdigit() and ch != '.']).strip()
-    value = float(value_str)# Convert value string to a float
-    
-    # Find the unit in the provided units list
-    try:
-        unit_index = units.index(unit_str)
-    except ValueError:
-        raise ValueError(f"Invalid unit '{unit_str}' in input string.")
-    
-    multiplier = 1024 ** unit_index # Calculate the multiplier based on the unit index
-    bytes = int(value * multiplier)   # Convert the human-readable value to bytes    
-    return bytes
-
-def bytes_to_human(bytes,units=['b','Kb','Mb','Gb','Tb','Pb']):
-    '''Converts an integer bytes into a human readable string notation
-    #TODO Different definitions for this and the other direction (1000 bytes per kb vs 1024)
-    
-    Args: 
-    bytes (int) : integer bytes
-    units (list) : unit convention for factors of 10 
-    
-    Returns (str) : the human readable string
-    '''
-
-    return str(bytes) + units[0] if bytes < 1024 else bytes_to_human(bytes>>10, units[1:])
 
 ##################################################################################################################################
 # Main Function
 def main():
-    base_data_dir = '/uufs/chpc.utah.edu/common/home/lin-group9/agm/NOAA_CSL' #where the data will be stored
-    check_space(base_data_dir,excep_thresh='6Tb') #ensure there is enough space in the director
-    ncd = NOAA_CSL_download(base_data_dir,bau_or_covid='COVID') #setup the downloader
-    for year in [2019,2020,2021]:
-        for month in range(1,13):
-            if (year==2021)&(month>8):
-                continue
-            for sector in ncd.sector_details.keys(): #loop through sectors
-                ncd.retrieve_format_data(sector,year,month)
+    import time
+    t1 = time.time()
+    base_data_storage_path = '/uufs/chpc.utah.edu/common/home/lin-group9/agm/NOAA_CSL_Data' #where the data will be stored
+    ncf.check_space(base_data_storage_path,excep_thresh='6Tb') #ensure there is enough space in the director
+    ncd = NOAA_CSL_download(base_data_storage_path,bau_or_covid='COVID') #setup the downloader
+    for sector in ncd.sector_details.keys(): #loop through sectors
+        ncd.retrieve_format_data(sector,2019,6)
+    t2 = time.time()
+    print(t1,t2,t2-t1)
+    # Full Download
+    # for year in [2019,2020,2021]:
+    #     for month in range(1,13):
+    #         if (year==2021)&(month>8):
+    #             continue
+    #         for sector in ncd.sector_details.keys(): #loop through sectors
+    #             ncd.retrieve_format_data(sector,year,month)
 
 if __name__ == "__main__":
     main()
---
+++ noaa_csl_funcs.py
@@ -0,0 +1,113 @@
+'''
+Module: noaa_csl_funcs.py
+Author: Aaron G. Meyer (agmeyer4@gmail.com)
+Last Updated: May 2, 2024
+Description:
+Useful functions for dealing with noaa csl data
+
+More information can be found here: https://csl.noaa.gov/groups/csl7/measurements/2020covid-aqs/emissions/
+'''
+
+##################################################################################################################################
+#Import Packages
+import os
+import subprocess
+import shutil
+import glob
+
+##################################################################################################################################
+# Define Functions
+def replace_all_strs(text,dic):
+    '''Replaces all strings contained in text matching the keys of dic with the values of dic
+    
+    Args:
+    text (str) : the string we want to replace stuff in
+    dic (dict) : a dictionary with keys as what should be replaced in text and values with what the key should be replaced with
+    
+    Returns :
+    text (str): the new text with keys replaced
+    '''
+
+    for key,value in dic.items():
+        text = text.replace(key,value)
+    return text
+
+def month_int_to_str(month_int):
+        '''Converts a month integer to a two character string
+        
+        Args: 
+        month_int (int) : the integer of the month (1,2,12,etc)
+        
+        Returns (str) : a string of form "MonthXX" where XX is the two digit month (1 --> Month01)'''
+
+        return f'Month{month_int:02d}'
+
+def check_space(path,excep_thresh='8Tb'):
+    '''Checks the amount of space on a filesystem given a path, and raise an error if the amount of space is below the threshold
+    
+    Args:
+    path (str) : path to check how much room there would be based on the filesystem
+    excep_thresh (str) : threshold below which to raise an exception, defaults '8T'
+    
+    Returns:
+    usage_details (tuple) : output of shutil.disk_usage(path)
+    
+    Raises:
+    MemoryError : if the amount of space on the filesystem containing the path is below the excep_thresh
+    '''
+
+    disk_usage = shutil.disk_usage(path)
+    total_bytes,used_bytes,free_bytes = int(disk_usage.total), int(disk_usage.used), int(disk_usage.free)
+    thresh_bytes = human_to_bytes(excep_thresh)
+    if free_bytes < thresh_bytes:
+        print(f'Path: {path}')
+        print(f'Total space: {bytes_to_human(total_bytes)}')
+        print(f'Used space: {bytes_to_human(used_bytes)}')
+        print(f'Free space: {bytes_to_human(free_bytes)}')
+        print(f'Required space (a user defined property): {excep_thresh}\n')
+        raise MemoryError(f'There is less than {excep_thresh} in the destination filesystem. Ensure there is enough room for your data download, or change the threshold')
+    else:
+        print(f'Sufficient space detected in {path}')
+        print(f'Found {bytes_to_human(free_bytes)} free space')
+        return disk_usage
+
+def human_to_bytes(human_readable, units=['b', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb']):
+    '''Makes a human readable storage size into a bytes integer
+    #TODO Different definitions for this and the other direction (1000 bytes per kb vs 1024)
+    
+    Args:
+    human_readable (str) : a human readable string representing space (something like 2.1Gb, or 5Tb)
+    units (list) : list of strings defining the units 
+    
+    Returns:
+    bytes (int) : integer representing how many bytes the human readable string is
+    '''
+
+    # Split the input string into value and unit
+    value_str = ''.join([ch for ch in human_readable if ch.isdigit() or ch == '.'])
+    unit_str = ''.join([ch for ch in human_readable if not ch.isdigit() and ch != '.']).strip()
+    value = float(value_str)# Convert value string to a float
+    
+    # Find the unit in the provided units list
+    try:
+        unit_index = units.index(unit_str)
+    except ValueError:
+        raise ValueError(f"Invalid unit '{unit_str}' in input string.")
+    
+    multiplier = 1024 ** unit_index # Calculate the multiplier based on the unit index
+    bytes = int(value * multiplier)   # Convert the human-readable value to bytes    
+    return bytes
+
+def bytes_to_human(bytes,units=['b','Kb','Mb','Gb','Tb','Pb']):
+    '''Converts an integer bytes into a human readable string notation
+    #TODO Different definitions for this and the other direction (1000 bytes per kb vs 1024)
+    
+    Args: 
+    bytes (int) : integer bytes
+    units (list) : unit convention for factors of 10 
+    
+    Returns (str) : the human readable string
+    '''
+
+    return str(bytes) + units[0] if bytes < 1024 else bytes_to_human(bytes>>10, units[1:])
+
